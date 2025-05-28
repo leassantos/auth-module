e4d57165ee1b8972ccc22509fe750a9bee617b9f
leassantos <lsts2005@gmail.com>
2025-05-28 15:24:19 -0300

    feat: Módulo de autenticação - estrutura inicial + Hello world

---
+++ src/test/java/com/leasantosbr/auth/AuthModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leasantosbr.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
